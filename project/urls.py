5a68914d56af59eb26ceb7eb629f3896207beb17
mahmoudmohamed57 <mahmoudmohamed572000@gmail.com>
2025-03-23 20:58:31 +0200

    final

--- project/urls.py
+++ project/urls.py
@@ -11,3 +11,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
